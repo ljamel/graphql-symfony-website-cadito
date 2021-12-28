4665e3f141e567133de87c25866db7989978e864
djamal lamri <contact@progfacil.com>
2021-12-28 10:48:01 +0100

    search correct price

--- src/Repository/ActivitysRepository.php
+++ src/Repository/ActivitysRepository.php
@@ -53,7 +53,7 @@ class ActivitysRepository extends ServiceEntityRepository
     public function chearch($search, $ville, $prices, $entityManager)
     {
         $prices = intval($prices);
-        $sql = "SELECT * FROM activitys WHERE MATCH (activitys.description) AGAINST ('$search' IN BOOLEAN MODE) and activitys.ville like '%$ville%' and activitys.prices > $prices ;";
+        $sql = "SELECT * FROM activitys WHERE MATCH (activitys.description) AGAINST ('$search' IN BOOLEAN MODE) and activitys.ville like '%$ville%' and activitys.prices >= $prices ;";
         $stmt = $entityManager->getConnection()->prepare($sql);
         $stmt->execute();
         $result = $stmt->executeQuery()->fetchAll();
