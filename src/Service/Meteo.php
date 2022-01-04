5a33c4f96752c113f54671ab1b4e96c4dfcc1523
djamal lamri <contact@progfacil.com>
2022-01-04 21:30:26 +0100

    Service meteo down

--- src/Service/Meteo.php
+++ src/Service/Meteo.php
@@ -5,6 +5,7 @@ class Meteo
 {
 public function getTemp(): string
 {
+    /*
     $data = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=68874e7171a98129f5640d2550f8783c345c97c7206509f2899a90f4ba7ed10c&insee=35238');
 
     if ($data !== false) {
@@ -14,6 +15,9 @@ public function getTemp(): string
 
         $meteo = $forecast->tmax;
     }
+    */
+
+    $meteo = 12;
 
 return $meteo;
 }
